99e6ec9a1cd369c80d135294a55edd30a6958041
HeryckMbs <68446973+HeryckMbs@users.noreply.github.com>
2022-06-05 01:49:59 -0300

    Update editar_prop.php

--- operacoes_prop/editar_prop.php
+++ operacoes_prop/editar_prop.php
@@ -11,6 +11,7 @@
 
 <body>
   <?php
+  // através da query seleciona o elemento a ser editado
   $result = mysqli_query($mysqli, "SELECT * FROM usuario_apt WHERE Cod_contrato=" . $_REQUEST["Cod_contrato"]);
   $dados = $result->fetch_object();
   ?>
@@ -18,11 +19,12 @@
     <form action="?page=salvar" method="POST">
 
       <input type="hidden" name="acao" value="editar">
+      // valor da chave primária do elemento a ser editado
       <input type="hidden" value="<?php print $dados->Cod_contrato; ?>" name="Cod_contrato">
       <h3>Você está alterando o contrato<br><strong> <?php print $dados->Cod_contrato; ?></strong></h3>
 
       <h3>Selecione o ID do apartamento</h3>
-
+    <!-- impressão dos dados nas options do select através do php-->
       <select class="selectform" name="Id_apt">
         <?php
         $result = mysqli_query($mysqli, "SELECT * FROM apartamento ORDER BY Id_Apt");
@@ -43,7 +45,7 @@
         <?php } ?>
       </select>
 
-
+<!-- validação JS-->
       <button onclick="valida(3)" class="btnform" type="submit ">Finalizar Edição</button>
       <br>
     </form>
