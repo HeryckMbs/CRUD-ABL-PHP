54fec396f1ba967d6ac9d50fe14a300deaf653f3
HeryckMbs <68446973+HeryckMbs@users.noreply.github.com>
2022-06-05 01:35:43 -0300

    Update lista-usuario.php

--- operacoes-usuario/lista-usuario.php
+++ operacoes-usuario/lista-usuario.php
@@ -2,11 +2,11 @@
 
 <?php
 
-
+//query para pegar todos os CPFs
 $result = mysqli_query($mysqli, "SELECT * FROM usuario ORDER BY CPF DESC");
 
 $rows_table = $result->num_rows;
-
+//impressão de tabela através do print
 if ($rows_table > 0) {
     print "<div class=cont-tab>";
     print "<table id='tabela-apt1' class='table table-dark table hover table-striped table-bordered border-success'>";
@@ -18,7 +18,8 @@ if ($rows_table > 0) {
     print "<th>Ação</th>";
     print "</tr>";
     $cont = 1;
-    while ($row = $result->fetch_object()) {
+    while ($row = $result->fetch_object()) { //irá retornar a linha atual do conjunto de resultados como um objeto aonde 
+                                             //os atributos do objeto representam os nomes dos campos encontrados no conjunto de resultados
         print "<tr>";
         print "<td>" . $cont . "</td>";
         print "<td>" . $row->CPF . "</td>";
@@ -34,6 +35,7 @@ if ($rows_table > 0) {
     }
     print "</table>";
 } else {
+    //validação de erro
     print "<script>alert('Não foram encontrados usuários cadastrados na base de dados')</script>";
     print "<script> location.href='?page=cadastro'; </script>";
 }
