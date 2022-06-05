b4ea31ffeb82b730673d7030ea272e3016eb8e34
HeryckMbs <68446973+HeryckMbs@users.noreply.github.com>
2022-06-05 01:58:03 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 
 <?php
-// Basic connection settings
+// configurações de conexão
 $databaseHost = 'localhost';
 $databaseUsername = 'root';
 $databasePassword = '';
 $databaseName = 'abl2';
 
-// Connect to the database
+// Conexão com o Banco
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
 ?>
