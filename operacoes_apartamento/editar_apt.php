ea9fe75b1106dc98ed0bf62a54f4640793ee93da
HeryckMbs <68446973+HeryckMbs@users.noreply.github.com>
2022-06-05 01:40:23 -0300

    Update editar_apt.php

--- operacoes_apartamento/editar_apt.php
+++ operacoes_apartamento/editar_apt.php
@@ -11,6 +11,7 @@
 
 <body>
   <?php
+  //query para selecionar especificamente o apartamento escolhido pelo usuário para ser alterado
   $res = mysqli_query($mysqli, "SELECT * FROM apartamento WHERE Id_Apt=" . $_REQUEST["Id_Apt"]);
   $dados = $res->fetch_object();
 
@@ -23,12 +24,12 @@
       <h1>Editar Usuário</h1>
       <input type="hidden" name="acao" value="editar">
       <input type="hidden" value="<?php print $dados->Id_Apt; ?>" name="Id_Apt">
-
+      <!-- Dados originais colocados nos inputs para que sejam alterados -->
       <input type="number" value="<?php print $dados->Id_Apt; ?>" name="Id_Apt" placeholder="Digite o ID apt">
       <input type="number" value="<?php print $dados->Id_bloco; ?>" name="Id_bloco" placeholder="Digite o ID do bloco">
       <input type="number" value="<?php print $dados->Num_andar; ?>" name="Num_andar" placeholder="Digite o n° do andar">
       <input type="number" value="<?php print $dados->Num_apt; ?>" name="Num_apt" placeholder="Digite o n° do apt">
-
+      <!-- validação JS-->
       <button onclick="valida(2)" class="btnform" type="submit ">Finalizar Edição</button>
     </form>
   </div>
