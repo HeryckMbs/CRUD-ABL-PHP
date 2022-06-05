1ab9e61b1c3a3f9e39ee725c2f6451347e9bae24
HeryckMbs <68446973+HeryckMbs@users.noreply.github.com>
2022-06-05 01:38:58 -0300

    Update cadastro_apt.php

--- operacoes_apartamento/cadastro_apt.php
+++ operacoes_apartamento/cadastro_apt.php
@@ -16,11 +16,12 @@
       <h1>Novo Apartamento</h1>
 
       <input type="hidden" name="acao" value="cadastrar">
+      <!-- Input para a inserção de dados na tabela Apartamento -->
       <input type="number" id="Id_Apt" name="Id_Apt" placeholder="Digite o ID do apt">
       <input type="number" id="Num_andar " name="Num_andar" placeholder="Digite o n° do andar">
       <input type="number" id="Num_apt" name="Num_apt" placeholder="Digite o n° do apt">
       <input type="number" id="Id_bloco" name="Id_bloco" placeholder="Digite o ID do bloco">
-
+      <!-- validação com JS -->
       <button onclick="valida_apt(2)" class="btnform" type="submit ">Finalizar Cadastro</button>
       <br>
     </form>
