011fabe4a824ffa4dcb6163782884771a9e39577
HeryckMbs <68446973+HeryckMbs@users.noreply.github.com>
2022-06-05 01:29:39 -0300

    Update cadastro_user.php

--- operacoes-usuario/cadastro_user.php
+++ operacoes-usuario/cadastro_user.php
@@ -14,12 +14,14 @@
 
     <form name="formuser" action="?page=salvar" method="POST">
       <h1>Novo Usuário</h1>
+      <!-- Envio da ação -->
       <input type="hidden" name="acao" value="cadastrar">
-
+      <!-- Inserção dos Dados no Input -->
       <input id="cpf" type="number" name="CPF" placeholder="Digite seu CPF sem . e -">
       <input id="nome" type="text" name="nome" placeholder="Nome">
       <input id="tel" type="tel" name="tel1" placeholder="Telefone 1">
-
+      
+      <!-- Validação com JS -->
       <button id="prop" onclick="validar(1)" class="btnform" type="submit ">Finalizar Cadastro</button>
       <script src="../script.js"></script>
 
