7ece9cc19412eab6d78557dcb511ecdfc2997460
HeryckMbs <68446973+HeryckMbs@users.noreply.github.com>
2022-06-05 01:44:54 -0300

    Update salva_apt.php

--- operacoes_apartamento/salva_apt.php
+++ operacoes_apartamento/salva_apt.php
@@ -1,4 +1,7 @@
 <?php
+//neste arquivo os dados serão alterados de fato
+//o switch irá controlar a operação recebida e direcionar para a operação correta
+//variaveis globais
     $Id_bloco = $_POST["Id_bloco"];
     $Num_andar = $_POST["Num_andar"];
     $Num_apt = $_POST["Num_apt"];
@@ -6,11 +9,11 @@
     switch(@$_REQUEST["acao"]){
     
     case 'cadastrar':
-
+//Insersão de Dados
         $stmt = $mysqli->prepare("INSERT INTO apartamento (Id_Apt,Num_andar,Num_apt,Id_Bloco) VALUES(?, ?, ?, ?) ");
         $stmt->bind_param("iiii", $Id_Apt,$Num_andar,$Num_apt,$Id_bloco);
         $stmt->execute();
-
+//validação de erro
         if($stmt == true){
             print "<script> alert('Cadastro realizado com sucesso'); </script>";
             print "<script> location.href='?page=listar'; </script>";
@@ -22,11 +25,11 @@
     break;
     
     case 'editar':
-    
+    //edição de dados
         $stmt = $mysqli->prepare("UPDATE apartamento SET Id_Apt=?,  Num_andar=?, Num_apt=?,Id_bloco=? WHERE Id_Apt=?");
         $stmt->bind_param("iiiii",  $Id_Apt,$Num_andar,$Num_apt,$Id_bloco, $Id_Apt,);
         $stmt->execute();
-
+//validação de erro
         if($stmt == true){
             print "<script> alert('Edição realizada'); </script>";
 
@@ -39,10 +42,11 @@
     break;
         
     case 'excluir':
-
+//exclusão de Dados
                 $stmt = $mysqli->prepare("DELETE FROM apartamento WHERE Id_Apt=?");
                 $stmt->bind_param("i", $_REQUEST["Id_Apt"]);
-                $stmt->execute();     
+                $stmt->execute();  
+//validação de erro
                 if($stmt == true){
                     print "<script> alert('Exclusão realizada'); </script>";
 
