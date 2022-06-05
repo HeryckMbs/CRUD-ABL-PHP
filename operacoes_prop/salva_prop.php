62d9f87181334fc104e5c1f488dd738b4a9736ad
HeryckMbs <68446973+HeryckMbs@users.noreply.github.com>
2022-06-05 01:55:18 -0300

    Update salva_prop.php

--- operacoes_prop/salva_prop.php
+++ operacoes_prop/salva_prop.php
@@ -1,15 +1,20 @@
-<?php
-$Id_Apt = $_POST["Id_Apt"];
-$CPF_usu = $_POST["CPF_usu"];
-$Cod_contrato = $_POST["Cod_contrato"];
+<?ph
+//utilizar variáveis globais aqui deram certo conflito então tive que coloca-las como locais
+//cadastro de proprietários de cada apartamento
+//manipulação real dos dados
+
+
 switch (@$_REQUEST["acao"]) {
     case 'cadastrar':
-
+        //inserção
+        $Id_Apt = $_POST["Id_Apt"];
+        $CPF_usu = $_POST["CPF_usu"];
+        $Cod_contrato = $_POST["Cod_contrato"];
 
         $stmt = $mysqli->prepare("INSERT INTO usuario_apt (Id_Apt,CPF_usu,Cod_contrato) VALUES(?,?,?) ");
         $stmt->bind_param("iii", $Id_Apt, $CPF_usu, $Cod_contrato);
         $stmt->execute();
-
+        //validação de erro
         if ($stmt == true) {
             print "<script> alert('Cadastro realizado com sucesso'); </script>";
         } else {
@@ -19,6 +24,7 @@ switch (@$_REQUEST["acao"]) {
         break;
 
     case 'editar':
+        //atualização de dados
         $Id_apt = $_POST["Id_apt"];
         $CPF_usu = $_POST["CPF_usu"];
         $Cod_contrato = $_POST["Cod_contrato"];
@@ -26,7 +32,7 @@ switch (@$_REQUEST["acao"]) {
         $stmt = $mysqli->prepare("UPDATE usuario_apt SET CPF_usu=?, Id_apt=?, Cod_contrato=?  WHERE Cod_contrato=?");
         $stmt->bind_param("iiii", $CPF_usu, $Id_apt, $Cod_contrato, $Cod_contrato);
         $stmt->execute();
-
+        //validação de erro
         if ($stmt == true) {
             print "<script> alert('Edição realizada'); </script>";
 
@@ -39,12 +45,14 @@ switch (@$_REQUEST["acao"]) {
         break;
 
     case "excluir":
+        //exclusão de dados
         $Id_Apt = $_POST["Id_Apt"];
         $CPF_usu = $_POST["CPF_usu"];
         $Cod_contrato = $_POST["Cod_contrato"];
         $stmt = $mysqli->prepare("DELETE FROM usuario_apt WHERE Cod_contrato=?");
         $stmt->bind_param("i", $_REQUEST["Cod_contrato"]);
         $stmt->execute();
+        //validação de erro
         if ($stmt == true) {
             print "<script> alert('Exclusão realizada'); </script>";
 
